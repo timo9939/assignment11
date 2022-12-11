2917dfc3988e5054b80e050f5a4e71486d32383b
timo9939 <timothylau20199@gmail.com>
2022-12-11 14:21:53 +0000

    3001 port initiate

--- miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/Develop/server.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const fs =require('fs');
+const path = require('path');
+const PORT = 3001;
+
+const app=express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+    // Send a message to the client
+    res.json(`${req.method} request received to get reviews`);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get reviews`);
+  });
+  
+  app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
